67cbde22f4733e5dee157a8da70665616a6ba9af
Sean Colsen <colsen.sean@gmail.com>
2023-12-27 09:21:56 -0500

    Begin using commands for CLI args

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,32 +1,40 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use querydown::*;
 use std::io::{self, Read};
 
+/// Querydown compiler
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-/// Querydown compiler
-struct Args {
-    #[arg(short, long)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Compile Querydown code to SQL
+    Compile(CompileArgs),
+    /// Analyze a database to generate a schema JSON file
+    Introspect,
+}
+
+#[derive(Debug, Args)]
+struct CompileArgs {
     /// Path to the schema JSON file
+    #[arg(short, long)]
     schema: String,
     /// The querydown query to execute. If empty, stdin will be used.
     query: Option<String>,
 }
 
-/// Get the query from the CLI argument if it exists, otherwise read it from stdin
-fn get_querydown_code(args: &mut Args) -> String {
-    if let Some(query) = std::mem::take(&mut args.query) {
-        query
-    } else {
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut buffer).unwrap();
-        buffer
-    }
+fn get_stdin() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    buffer
 }
 
-fn main() -> () {
-    let mut args = Args::parse();
-    let querydown_code = get_querydown_code(&mut args);
+fn compile(args: CompileArgs) {
+    let querydown_code = args.query.unwrap_or_else(get_stdin);
     let schema_json = std::fs::read_to_string(args.schema).unwrap();
     let options = Options {
         dialect: Box::new(Postgres()),
@@ -35,4 +43,16 @@ fn main() -> () {
     let compiler = Compiler::new(&schema_json, options).unwrap();
     let sql_code = compiler.compile(querydown_code).unwrap();
     println!("{sql_code}");
+}
+
+fn introspect() {
+    todo!()
+}
+
+fn main() -> () {
+    let args = Cli::parse();
+    match args.command {
+        Command::Compile(args) => compile(args),
+        Command::Introspect => introspect(),
+    }
 }
